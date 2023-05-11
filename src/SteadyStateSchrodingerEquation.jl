2d9a1597c2e87bf9396ebb38b099a2350e8f9ea9
mendzmartin <chmartinmendez@gmail.com>
2023-05-11 15:56:56 -0300

    Building Functions

--- src/SteadyStateSchrodingerEquation.jl
+++ src/SteadyStateSchrodingerEquation.jl
@@ -1,13 +1,21 @@
 module SteadyStateSchrodingerEquation
 
-# Write your package code here.
+using Gridap
+using GridapGmsh
+using Gmsh
+using Gridap.CellData  # para construir condición inicial interpolando una función conocida
+using Gridap.FESpaces  # para crear matrices afines a partir de formas bilineales
+using Gridap.Algebra   # para utilizar operaciones algebraicas con Gridap
+using FileIO
+using LinearAlgebra
+using SparseArrays
+using SuiteSparse
+using Arpack
 
 include("Functions/MeshGeneratorFunction.jl")
 include("Functions/BoundaryConditionsFunction.jl")
 include("Functions/MiscellaneousFunctions.jl")
 include("Functions/EigenProblemDefinitionFunction.jl")
 include("Functions/EigenProblemSolveFunction.jl")
-
-
 
 end

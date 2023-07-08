e4689105bf0547ab737f8c13b40fe600f8802a63
mendzmartin <chmartinmendez@gmail.com>
2023-07-08 09:53:55 -0300

    fix bugs workflow

--- docs/make.jl
+++ docs/make.jl
@@ -6,20 +6,22 @@ push!(LOAD_PATH,"../src/")
 using TimeIndependentSchrodingerEquation
 using Documenter
 
+# makedocs(
+#          sitename = "TimeIndependentSchrodingerEquation.jl",
+#          modules  = [TimeIndependentSchrodingerEquation],
+#          pages=[
+#                 "Home" => "index.md"
+#                ])
+
 makedocs(
-         sitename = "TimeIndependentSchrodingerEquation.jl",
-         modules  = [TimeIndependentSchrodingerEquation],
-         pages=[
-                "Home" => "index.md"
-               ])
+    sitename = "TimeIndependentSchrodingerEquation.jl",
+    modules  = [TimeIndependentSchrodingerEquation],
+    pages = ["Home" => "index.md"],
+    format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    assets = String[],),
+    )
 
 deploydocs(;
     repo="github.com/mendzmartin/TimeIndependentSchrodingerEquation.jl",
-)
-
-# using Pkg
-# Pkg.activate("../")
-
-# using Documenter
-# using TimeIndependentSchrodingerEquation
-# makedocs(sitename="My Documentation")
+    )

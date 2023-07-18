0a02237b774c84b123b20f40054728ef5ab8167d
mendzmartin <chmartinmendez@gmail.com>
2023-07-18 18:51:20 -0300

    add actions

--- docs/make.jl
+++ docs/make.jl
@@ -1,27 +1,43 @@
-using Pkg
-Pkg.activate("../")
+# using Pkg
+# Pkg.activate("../")
 
-push!(LOAD_PATH,"../src/")
+# push!(LOAD_PATH,"../src/")
 
-using TimeIndependentSchrodingerEquation
-using Documenter
+# using TimeIndependentSchrodingerEquation
+# using Documenter
+
+# # makedocs(
+# #          sitename = "TimeIndependentSchrodingerEquation.jl",
+# #          modules  = [TimeIndependentSchrodingerEquation],
+# #          pages=[
+# #                 "Home" => "index.md"
+# #                ])
 
 # makedocs(
-#          sitename = "TimeIndependentSchrodingerEquation.jl",
-#          modules  = [TimeIndependentSchrodingerEquation],
-#          pages=[
-#                 "Home" => "index.md"
-#                ])
+#     sitename = "TimeIndependentSchrodingerEquation.jl",
+#     modules  = [TimeIndependentSchrodingerEquation],
+#     pages = ["Home" => "index.md"],
+#     format = Documenter.HTML(;
+#     prettyurls = get(ENV, "CI", "false") == "true",
+#     assets = String[],),
+#     )
+
+# deploydocs(;
+#     repo="github.com/mendzmartin/TimeIndependentSchrodingerEquation.jl.git",
+#     )
+
+using Documenter
+using TimeIndependentSchrodingerEquation
 
 makedocs(
-    sitename = "TimeIndependentSchrodingerEquation.jl",
-    modules  = [TimeIndependentSchrodingerEquation],
-    pages = ["Home" => "index.md"],
-    format = Documenter.HTML(;
-    prettyurls = get(ENV, "CI", "false") == "true",
-    assets = String[],),
-    )
+    sitename = "TimeIndependentSchrodingerEquation",
+    format = Documenter.HTML(),
+    modules = [TimeIndependentSchrodingerEquation]
+)
 
-deploydocs(;
-    repo="github.com/mendzmartin/TimeIndependentSchrodingerEquation.jl.git",
-    )
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+deploydocs(
+    repo = "github.com/mendzmartin/TimeIndependentSchrodingerEquation.jl.git"
+)
